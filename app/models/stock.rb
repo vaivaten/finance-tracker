7f8a7190fbeb82c0e3ccbc62e89028198947cd27
Vaiva <vaivaa.t@gmail.com>
2018-07-09 10:34:30 +0300

    User and Stocks associations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
   def self.new_from_lookup(ticker_symbol)
     begin
@@ -8,4 +10,9 @@ class Stock < ApplicationRecord
       return nil
     end
   end
+
+  def self.find_by_ticker(ticker_symbol)
+   where(ticker: ticker_symbol).first
+ end
+
 end

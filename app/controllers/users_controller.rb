66b4247435d343a6f5ce329c45a77439071d1ba5
Vaiva <vaivaa.t@gmail.com>
2018-07-12 11:09:30 +0300

    Search for friends and add them to your list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,20 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.search(params[:search_param])
-    render json:@users
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No such user" if @users.blank?
+    end
+
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+
   end
 end
